47adb3a6f955d668924a7922fe2ab700236b484e
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-06-15 18:03:39 -0300

    feat(client): Create Client Service

--- src/modules/client/useCases/createClient/create-client.service.ts
+++ src/modules/client/useCases/createClient/create-client.service.ts
@@ -1,7 +1,27 @@
 import ClientRepositoryInterface from 'modules/client/interfaces/client-repository.interface';
+import ClientInterface from 'modules/client/interfaces/client.interface';
+import * as bcrypt from 'bcryptjs';
+import CreateClientDto from './dtos/create-client.dto';
 
 export default class CreateClientService {
   constructor(private readonly clientRepository: ClientRepositoryInterface) {}
 
-  execute() {}
+  async execute(createClientDto: CreateClientDto): Promise<ClientInterface> {
+    const clientExists = await this.clientRepository.exists(
+      createClientDto.cnpj,
+    );
+
+    if (clientExists) {
+      throw new Error('Client already exists!');
+    }
+
+    const passwordHash = await bcrypt.hash(createClientDto.password, 10);
+
+    const newClient = await this.clientRepository.create({
+      ...createClientDto,
+      password: passwordHash,
+    });
+
+    return newClient;
+  }
 }
