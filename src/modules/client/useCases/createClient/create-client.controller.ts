70fc8d770921b3e5be10556073632e4ba934153e
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-06-15 18:07:55 -0300

    feat(client): Create Client Controller was implemented

--- src/modules/client/useCases/createClient/create-client.controller.ts
+++ src/modules/client/useCases/createClient/create-client.controller.ts
@@ -5,6 +5,24 @@ export default class CreateClientController {
   constructor(private readonly createClientService: CreateClientService) {}
 
   async handle(request: Request, response: Response) {
-    return response.json({});
+    try {
+      const { name, cnpj, email, password } = request.body;
+
+      const newClient = await this.createClientService.execute({
+        name,
+        cnpj,
+        email,
+        password,
+      });
+
+      return response.json({
+        ...newClient,
+        password: undefined,
+      });
+    } catch (error: any) {
+      response.status(400).json({
+        message: error?.message,
+      });
+    }
   }
 }
